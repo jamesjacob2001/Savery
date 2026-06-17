0e96b81355a18f9b01ab6781f83e52875d3c4cce
mrejuan3 <melissarejuan@gmail.com>
2026-06-17 19:45:41 -0400

    set up nodeman for automatic restart of server

--- backend.js
+++ backend.js
@@ -3,9 +3,12 @@ import express from "express";
 console.log("Savery server starting...");
 console.log("Hello from the backend...");
 
-const app = express();
 // check if port is in environment variables, if not use 3000
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("frontend"));
 
 // this function is being called by express to start the server
 app.listen(PORT, () => {
